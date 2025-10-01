ba173b8896ad2d2edab9d97d8f43b6dc6fd4b923
Danilo Rodrigues <danilo@fillet.com.br>
2025-10-01 17:34:45 -0300

    Configured jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/packages', '<rootDir>/examples'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  moduleNameMapper: {
+    '^@kiqjs/core(.*)$': '<rootDir>/packages/core/src$1',
+    '^@kiqjs/http(.*)$': '<rootDir>/packages/http/src$1',
+    '^@kiqjs/repository(.*)$': '<rootDir>/packages/repository/src$1',
+  },
+  setupFilesAfterEnv: [],
+};
